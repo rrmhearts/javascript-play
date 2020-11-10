26be152fd2d08ed16b6efc466e42a436f37553f4
Ryan <rrmhearts@gmail.com>
2020-11-10 13:46:42 -0500

    drag and drop examples. basic testing global vars..

---
+++ basic/app.js
@@ -0,0 +1,6 @@
+// Witchcraft from global var (window.autoGet)
+autoGet.innerHTML = "I grabbed this Element by witchcraft!"
+
+let autoGetNormal = document.getElementById('autoGet');
+
+autoGetNormal.innerHTML = "Now I got it more traditionally.."
