33c5f14f1d51a0ba77f271ebfc6ca5b4e55e7f47
Ryan <rrmhearts@gmail.com>
2020-11-13 13:47:48 -0500

    graphql example.

--- basic/app.js
+++ basic/app.js
@@ -4,3 +4,16 @@ autoGet.innerHTML = "I grabbed this Element by witchcraft!"
 let autoGetNormal = document.getElementById('autoGet');
 
 autoGetNormal.innerHTML = "Now I got it more traditionally.."
+
+fetch("http://localhost:4000/graphql", {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    method: 'POST',
+    body: JSON.stringify({query: "{ hello }"})
+}).then(res => res.json())
+  .then(data => {
+      console.log(data);
+      graphP.innerHTML=data.data.hello
+  })
