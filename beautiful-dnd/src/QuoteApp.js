ac43f4ee75e34aaf41aebb6d0b1397d4862b9a1b
Ryan <rrmhearts@gmail.com>
2020-11-16 14:17:40 -0500

    Some comments.

--- beautiful-dnd/src/QuoteApp.js
+++ beautiful-dnd/src/QuoteApp.js
@@ -3,19 +3,20 @@ import styled from "@emotion/styled";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // Create new array using key. Complicated way around..
-const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
-  const custom = {
+const initial = Array.from({ length: 10 }, (v, k) => {
+// const initial = new Array(10).fill(0).map((v, k) => {
+  return {
     id: `id-${k}`,
     content: `Quote ${k}`
   };
-
-  return custom;
 });
 
 const grid = 8;
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
+  // remove the startIndex item
   const [removed] = result.splice(startIndex, 1);
+  // add that item to endIndex in array.
   result.splice(endIndex, 0, removed);
 
   return result;
@@ -31,9 +32,10 @@ const QuoteItem = styled.div`
 
 function Quote({ quote, index }) {
   return (
+    // A draggable bar.. has unique id
     <Draggable draggableId={quote.id} index={index}>
-      {provided => (
-        <QuoteItem
+      {provided => ( // draggable setup HOC
+        <QuoteItem // The actual item.
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
@@ -46,7 +48,7 @@ function Quote({ quote, index }) {
 }
 
 const QuoteList = React.memo(function QuoteList({ quotes }) {
-  return quotes.map((quote: QuoteType, index: number) => (
+  return quotes.map((quote/*: QuoteType*/, index/*: number*/) => (
     <Quote quote={quote} index={index} key={quote.id} />
   ));
 });
@@ -63,6 +65,7 @@ function QuoteApp() {
       return;
     }
 
+    // When dragging complete, update array.
     const quotes = reorder(
       state.quotes,
       result.source.index,
@@ -77,6 +80,7 @@ function QuoteApp() {
       <Droppable droppableId="list">
         {provided => (
           <div ref={provided.innerRef} {...provided.droppableProps}>
+            {/* The wrapped content is an array of quotes */}
             <QuoteList quotes={state.quotes} />
             {provided.placeholder}
           </div>
@@ -86,5 +90,3 @@ function QuoteApp() {
   );
 }
 export default QuoteApp;
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<QuoteApp />, rootElement);
